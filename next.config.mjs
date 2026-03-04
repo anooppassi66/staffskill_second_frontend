10a46fe96ba466bf4929ddb18aedf1c3261e5d5b
Mohd Atish <atishmohd7786@gmail.com>
2026-03-04 13:55:36 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   // when the app is deployed under a sub‑path (e.g. https://neurocruit.ai/lms)
   // we need to let Next know so that routing and asset paths are adjusted.
-  basePath: '/lms',
-  assetPrefix: '/lms/',
 
   typescript: {
     ignoreBuildErrors: true,
